3aee04d57391f5531fb70eeefebcec6031820fd2
Tcoi9428 <tcoi9428@github.com>
2020-11-04 22:11:04 +0600

    create parser

---
+++ json-parser/dist/parser.js
@@ -0,0 +1,26 @@
+let items = [];
+
+
+let catalogItem = document.querySelectorAll('.js-catalog-item');
+    catalogItem.forEach(function (item){
+            let itemId = item.getAttribute('data-id');
+            let itemName = item.getAttribute('data-name');
+            let itemType = item.getAttribute('data-type');
+            let itemLink = item.querySelector('a').getAttribute('href');
+            let itemImage = item.querySelector('img').getAttribute('src');
+            let itemThumbnail = item.querySelector('img').getAttribute('src');
+                site = {
+                    id   : itemId,
+                    name : itemName,
+                    type : itemType,
+                    link : itemLink,
+                    thumbnail : itemThumbnail,
+                    image : itemImage,
+                }
+        items.push(site);
+        }
+    )
+json = JSON.stringify(items);
+var newWin = window.open("about:blank");
+    newWin.document.write(json);
+
